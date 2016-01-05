c54618b454ef703c2a975051078ef32f78875309
huguodong <531035580@QQ.COM>
2016-01-05 12:52:06 +0800

    无

---
+++ samples/RoundedImageViewSample/RoundedImageViewSample/obj/Debug/android/src/md500fd750c60df5f9d6eb2c0d3de1b1520/PicassoFragment.java
@@ -0,0 +1,46 @@
+package md500fd750c60df5f9d6eb2c0d3de1b1520;
+
+
+public class PicassoFragment
+	extends android.support.v4.app.Fragment
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("RoundedImageViewSample.PicassoFragment, RoundedImageViewSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", PicassoFragment.class, __md_methods);
+	}
+
+
+	public PicassoFragment () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == PicassoFragment.class)
+			mono.android.TypeManager.Activate ("RoundedImageViewSample.PicassoFragment, RoundedImageViewSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
